e51ccddcdbedc20d28d995d734ac691e72715892
Adrian Mohabir <mohabiradrian@gmail.com>
2020-01-10 20:52:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
     if @side_one <= 0 || @side_one + @side_two <= @side_three
       raise TriangleError
     else 
-      if @side_one + @side_two < @side_three || 
+      if @side_one + @side_two < @side_three || @side_one + @side_three < @side_two
     elsif 
     @side_one == @side_two && @side_two == @side_three 
     return :equilateral 
